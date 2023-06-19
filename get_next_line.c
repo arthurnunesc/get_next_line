4c352ec54af38cebbc25ed89845c94eb23a4f1d6
Arthur Nunes <arthurnunesc@proton.me>
2023-06-19 17:33:08 +0200

    Brake everything again

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,55 @@
 /*   By: anunes-c <anunesc-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 14:37:49 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/06/14 14:50:27 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/06/19 14:56:30 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+static char	*ft_fillstash(char *stash, char *buffer)
+{
+	char	*new_stash;
+	char	*clean_buffer;
+
+	clean_buffer = ft_substr(buffer, 0, BUFFER_SIZE);
+	if (!clean_buffer)
+		return (0);
+	if (!stash || stash[0] == '\0')
+	{
+		new_stash = clean_buffer;
+	}
+	else
+	{
+		new_stash = ft_strjoin(stash, clean_buffer);
+		free(stash);
+		if (!new_stash)
+			return (0);
+	}
+	return (new_stash);
+}
+
+static char	*ft_cleanstash(char *stash, ssize_t line_len)
+{
+	char	*new_stash;
+	ssize_t	stash_len;
+
+	stash_len = ft_strlen(stash);
+	new_stash = ft_substr(stash, line_len + 1, stash_len - line_len);
+	free(stash);
+	if (!new_stash)
+		return (0);
+	return (new_stash);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*stash;
 	char		*line;
-	char		*aux;
 	char		buffer[BUFFER_SIZE];
 	ssize_t		read_output;
 	ssize_t		line_len;
-	ssize_t 	stash_len;
 	int			has_reached_eoloreof;
 
 	if (fd < 0)
@@ -31,19 +64,15 @@ char	*get_next_line(int fd)
 	has_reached_eoloreof = 0;
 	while (!has_reached_eoloreof)
 	{
-		read_output = read(fd, buffer, sizeof(buffer));
+		read_output = read(fd, buffer, BUFFER_SIZE);
 		if (read_output == -1)
 			return (0);
-		printf("UNCLEAN BUFFER: %s\n", buffer);
+		printf("BUFFER: %s\n", buffer);
 		// printf("READ OUTPUT: %zi\n", read_output);
-		if (ft_strchr(buffer, '\n') || read_output < (ssize_t) sizeof(buffer))
+		if (ft_strchr(buffer, '\n') || read_output < BUFFER_SIZE)
 			has_reached_eoloreof = 1;
-		aux = ft_substr(buffer, 0, read_output);
-		if (!aux)
-			return (0);
-		printf("CLEAN BUFFER(AUX): %s\n", aux);
 		printf("INTERMEDIARY STASH BEFORE: %s\n", stash);
-		stash = ft_strjoin(stash, aux);
+		stash = ft_fillstash(stash, buffer);
 		if (!stash)
 			return (0);
 		printf("INTERMEDIARY STASH AFTER: %s\n", stash);
@@ -51,21 +80,15 @@ char	*get_next_line(int fd)
 	// printf("FINAL STASH: %s\n", stash);
 	line = 0;
 	line_len = 0;
-	stash_len = 0;
 	while (stash[line_len] && stash[line_len] != '\n')
 		line_len++;
-	while (stash[stash_len])
-		stash_len++;
-	// printf("LINE_LEN: %lu\n", line_len);
-	// printf("STASH_LEN: %lu\n", stash_len);
-	// printf("STASH_LEN - LINE_LEN: %lu\n", (stash_len - line_len));
+	printf("LINE_LEN: %lu\n", line_len);
 	line = ft_substr(stash, 0, line_len);
-	stash = ft_substr(stash, line_len + 1, stash_len);
+	stash = ft_cleanstash(stash, line_len);
 	if (!stash)
 		return (0);
 	if (!line)
 		return (0);
-	// printf("LINE: %s\n", line);
 	printf("LEFTOVER STASH: %s\n", stash);
 	return (line);
 }
